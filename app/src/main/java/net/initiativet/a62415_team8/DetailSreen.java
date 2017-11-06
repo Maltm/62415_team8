85d3b12e227b193bbcb34f805bbb242f4075505d
malte <malte_1994@hotmail.co>
2017-11-06 13:01:22 +0100

    Tilføjet textview til detail_screen

--- app/src/main/java/net/initiativet/a62415_team8/DetailSreen.java
+++ app/src/main/java/net/initiativet/a62415_team8/DetailSreen.java
@@ -3,8 +3,11 @@ package net.initiativet.a62415_team8;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class DetailSreen extends AppCompatActivity implements View.OnClickListener {
 
@@ -12,9 +15,12 @@ public class DetailSreen extends AppCompatActivity implements View.OnClickListen
     String title = "";
     String resume = "";
     String nummer = "";
+    String url = "";
     TextView titleTextView;
     TextView resumeTextView;
+    TextView ftLink;
     ImageButton backButton;
+    Button propAsProposed, propAsAdopted, goToVote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +36,19 @@ public class DetailSreen extends AppCompatActivity implements View.OnClickListen
         backButton.setBackgroundResource(R.drawable.arrow_left);
         backButton.setOnClickListener(this);
 
+        propAsProposed = (Button) findViewById(R.id.propAsProposed);
+        propAsAdopted = (Button) findViewById(R.id.propAsAdopted);
+        goToVote = (Button) findViewById(R.id.goToVote);
+
+        propAsAdopted.setOnClickListener(this);
+        propAsProposed.setOnClickListener(this);
+        goToVote.setOnClickListener(this);
+
         titleTextView = (TextView) findViewById(R.id.detail_title);
         resumeTextView = (TextView) findViewById(R.id.detail_resume);
+        ftLink = (TextView) findViewById(R.id.ftLink);
+
+        ftLink.setText("For flere detajler se link: ");
 
         titleTextView.setText(nummer +  " " + title);
         resumeTextView.setText(resume);
@@ -43,6 +60,12 @@ public class DetailSreen extends AppCompatActivity implements View.OnClickListen
     public void onClick(View view) {
         if(view == backButton){
             finish();
+        } else if (view == propAsProposed) {
+            url = "http://";
+        } else if(view == propAsAdopted) {
+
+        } else if(view == goToVote) {
+
         }
     }
 }
